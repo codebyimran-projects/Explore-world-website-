55b9b1c41de51475608fcb835742ceb4d18b2f7a
Muhammd imran <muhammadimran171825@gmail.com>
2025-05-14 23:44:32 +0500

    ssecond commit

--- javascript/home.js
+++ javascript/home.js
@@ -11,3 +11,24 @@
     mobileMenu.classList.remove('show');
   });
 
+
+
+
+  // hero section 
+
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.hero-slide');
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle('active', i === index);
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  setInterval(nextSlide, 6000); // Change slide every 6 seconds
+
